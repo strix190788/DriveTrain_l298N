a52c8829d21ff7e60378196f70c2aa5a3f447c72
strix190788 <strix190788@gmail.com>
2026-03-15 15:20:10 +0400

    Update driveTrain.h

--- driveTrain.h
+++ driveTrain.h
@@ -1,22 +1,74 @@
 class driveTrain {
 public:
-  driveTrain(int in1, int in2, int in3, int in4) {
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-    _in1 = in1;
-    _in2 = in2;
-    _in3 = in3;
-    _in4 = in4;
+  driveTrain(byte in1, byte in2, byte in3, byte in4) {
+    motor[0][0] = in1;
+    motor[0][1] = in2;
+    motor[1][0] = in3;
+    motor[1][1] = in4;
+    pinMode(motor[0][0], OUTPUT);
+    pinMode(motor[1][0], OUTPUT);
+    pinMode(motor[1][1], OUTPUT);
+    pinMode(motor[0][1], OUTPUT);
   }
-  void move(int leftpwr, int rightpwr){
-    analogWrite(_in1, leftpwr);
-    digitalWrite(_in2, 0);
+  void reverseLeftMotor() {
+    motor[0][2] = not motor[0][2];
+  }
+  void reverseRightMotor() {
+    motor[1][2] = not motor[1][2];
+  }
 
-    analogWrite(_in3, rightpwr);
-    digitalWrite(_in4, 0);
+  void stop() {
+    digitalWrite(motor[0][0], 0);
+    digitalWrite(motor[0][1], 0);
+	digitalWrite(motor[1][0], 0);
+    digitalWrite(motor[1][1], 0);
+    delay(50);
   }
+
+  void move(int pwrLeft, int pwrRight) {
+    pwrLeft = constrain(pwrLeft, -255, 255);
+    pwrRight = constrain(pwrRight, -255, 255);
+    if (motor[0][2]==1) pwrLeft = pwrLeft * (-1);
+    if (motor[1][2]==1) pwrRight = pwrRight * (-1);
+	
+    if (pwrLeft > 0) {
+      
+        digitalWrite(motor[0][0], 0);
+        analogWrite(motor[0][1], pwrLeft);
+      } else {
+        digitalWrite(motor[0][1], 0);
+        analogWrite(motor[0][0], abs(pwrLeft));
+      }
+    
+    if (pwrRight > 0) {
+      
+        digitalWrite(motor[1][0], 0);
+        analogWrite(motor[1][1], pwrRight);
+      } 
+	  else {
+        digitalWrite(motor[1][1], 0);
+        analogWrite(motor[1][0], abs(pwrRight));
+    }
+    
+  }
+
+  void move(byte pwrLeft, byte pwrRight, int millsec) {
+    move(pwrLeft, pwrRight);
+    delay(millsec);
+    stop();
+  }
+
+  void reverse() {
+    for (int i = 0; i < 3; i++) {
+      byte a = motor[0][i];
+      motor[0][i] = motor[1][i];
+      motor[1][i] = a;
+    }
+    reverseLeftMotor();
+    reverseRightMotor();
+  }
+
 private:
-  int _in1, _in2, _in3, _in4;
+  byte _in1, _in2, _in3, _in4;
+  byte motor[2][3] = { {0,0,0}, {0,0,0} };
 };
